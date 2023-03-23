79c9a78d2363f8fff5ba4a9a1c8495279a6eb351
Mounir <polomnr1@gmail.com>
2023-03-23 14:56:05 +0100

    send content to discord-webhook with help of ngrok

--- server.js
+++ server.js
@@ -6,12 +6,47 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.get('/', (req, res) => {`
-    <html>
-        <head><title>My App</title></head>
-        <body>
-            <h1>My App</h1>
-            <p>My App is running</p>
-        </body>
-    </html>
-`}
+app.get('/', (req, res) => res.send(`
+        <html lang="en">
+            <head><title>Success boi!</title></head>
+            <body>
+                <h1>Zaaamn daniel</h1>
+                <img src="https://media.giphy.com/media/XreQmk7ETCak0/giphy.gif" alt="Cool kid doing thumbs up" />
+            </body>
+        </html>
+    `));
+
+
+app.post("/github", (req, res) => {
+    console.log(req.body)
+    action = req.body.action
+    const content = `:wave: sup nigs! ${req.body.sender.login} just ${action} to/from ${req.body.repository.full_name}!`;
+    const avatarUrl = action === 'started' ? "https://media.giphy.com/media/3o7TKSjRrfIPjeiVyM/giphy.gif" : "https://media.giphy.com/media/W0c3xcZ3F1d0EYYb0f/giphy.gif"
+    axios
+        .post(process.env.DISCORD_WEBHOOK_URL, {
+            content: content,
+            embeds: [
+                {
+                    image: {
+                        url: avatarUrl,
+                    },
+                },
+            ],
+        })
+        .then((discordResponse) => {
+            console.log("Success!");
+            res.status(204).send(discordResponse);
+        })
+        .catch((err) => console.error(`Error sending to Discord: ${err}`));
+});
+
+app.use((error, req, res, next) => {
+    res.status(500)
+    res.send({error: error})
+    console.error(error.stack)
+    next(error)
+})
+
+app.listen(port, () =>
+    console.log(`Example app listening at http://localhost:${port}`)
+);
